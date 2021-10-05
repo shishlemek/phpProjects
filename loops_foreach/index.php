21315216f39e8ef7d7ecd7657c09ec1731efb0b4
dimitar-13 <dimitarnaidenov5@gmail.com>
2021-10-05 16:25:19 +0300

    Add files

--- loops_foreach/index.php
+++ loops_foreach/index.php
@@ -8,36 +8,34 @@
   <body>
     <h3>This is example for foreach <br></h3>
     <?php
-    $arr = [
-      'John Smith' => '+1-555-8976',
-      'Lisa Smith' => '+1-555-1234',
-      'Sam Doe' => '+1-555-5030',
-           ];
-           print_r($arr);
+      $arr = [
+        'John Smith' => '+1-555-8976',
+        'Lisa Smith' => '+1-555-1234',
+        'Sam Doe' => '+1-555-5030',
+      ];
+      print_r($arr);
     ?>
-<h3>This is another example for foreach <br></h3>
+    <h3>This is another example for foreach <br></h3>
     <?php
-
       $num = 1;
         $key = [
           '+1-555-5030',
         ];
-      foreach($key as $value){
-      echo 'Phone number No'. $num++ .'is' . $value;
+      foreach($key as $value) {
+        echo 'Phone number No' . $num++ .'is' . $value;
       }
-      foreach($key as $phone){
-      echo 'Phone number No'. $num .'is' . $phone;
-      $num++;
+      foreach($key as $phone) {
+        echo 'Phone number No' . $num .'is' . $phone;
+        $num++;
       }
     ?>
     <h3>While example  <br></h3>
     <?php
-    $x = 1;
-    while($x < 6){
-    echo ' <br> The number is ' .
-    $x;
-    $x++;
-    }
+      $x = 1;
+      while($x < 6) {
+        echo ' <br> The number is ' . $x;
+        $x++;
+      }
     ?>
   </center>
 
--- nestedLoop/index.php
+++ nestedLoop/index.php
@@ -1,14 +1,58 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    for ($x = 0; $x <= 10; $x++) {
-    echo "<p> The number is: $x </p>";
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php
+    for ($x = 0 ; $x <= 10; $x++) {
+      echo "<p> The number is: $x </p>";
     }
-    ?>
-  </body>
+
+    for ($x = 1; $x <= 10; $x++) {
+      echo ' <p> No' . $x . ' : ' ;
+      for ($y = 1; $y <= 5; $y++) {
+        echo $y . ' , ';
+      }
+      echo ' </p> ';
+    }
+
+    $arName = [
+      'name' => 'Gosho Petrov',
+      'family_members' =>[
+        'wife' => 'Pepa Petrova',
+        'son' => 'Ivan Petrov',
+        'daughter'=> 'Ceca Petrova',
+        'dauther_boyfriend' => 'Pesho'
+      ],
+      '<br>',
+      'address' => [
+        'city' => 'Vratsa',
+        'street' => 'Kokiche',
+        'street_number' => 14,
+      ],
+    ];
+    print_r($arName);
+
+    if ($_POST) {
+      echo '<pre>';
+      echo htmlspecialchars(print_r($_POST, true));
+      echo '</pre>';
+    }
+  ?>
+  <center>
+    <form action="" method="post">
+      Name:  <input type="text" name="personal[name]" /><br />
+      Email: <input type="text" name="personal[email]" /><br />
+      Beer: <br />
+      <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
+      </select><br />
+      <input type="submit" value="submit me!" />
+    </center>
+  </form>
+</body>
 </html>
