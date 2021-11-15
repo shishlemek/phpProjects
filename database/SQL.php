bd5ad264326e8393552ea9499530a4fe2d1079ae
shishlemek <83347720+shishlemek@users.noreply.github.com>
2021-11-15 09:53:04 +0200

    nz

--- database/SQL.php
+++ database/SQL.php
@@ -6,38 +6,48 @@
 <body>
   <?php
 
-  $db = mysqli_connect("localhost","root","","shop");
+  $db = mysqli_connect("localhost","root","","shop_v1");
 
   if(!$db)
   {
     die("Connection failed: " . mysqli_connect_error());
   }
 
-  if(isset($_POST['submit']))
+  if(isset($_POST['cart_id']))
   {
     $product_name = $_POST['product_name'];
+    $cart_id = $_POST['cart_id'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
 
-    $insert = mysqli_query($db,"INSERT INTO `products`(`product_name`) VALUES ('$product_name')");
+    $insert = mysqli_query($db,"INSERT INTO `products`(`name`,`cart_id`,`quantity`,`price`) VALUES ('$product_name','$cart_id','$quantity','$price')");
 
-    if(!$insert)
-    {
-      echo mysqli_error();
-    }
-    else
-    {
+    if($insert){
       echo "Records added successfully.";
     }
   }
 
-  mysqli_close($db);
   ?>
 
   <h3>Product</h3>
 
   <form method="POST">
-    Product Name : <input type="text" name="product name" placeholder="Enter product name" Required>
-    <br/>
-    <input type="submit" name="submit" value="Submit">
-  </form>
+    Product Name : <input type="text" name="product name" placeholder="Enter product name" required>
+    <select>
+   <option disabled selected>-- Select product --</option>
+   <?php
+       $records = mysqli_query($db, "SELECT `cart_id` From `products`");
+
+       while($db = mysqli_fetch_array($records))
+       {
+           echo "<option value='". $db['cart_id'] ."'>" .$db['cart_id'] ."</option>";
+       }
+   ?>
+ </select>
+  <input type="number " name="quantity" placeholder="quantity">
+  <input type="number " name="price" placeholder="price">
+  <input type="submit" name="submit" value="Submit">
+</form>
+<?php mysqli_close($db);?>
 </body>
 </html>
